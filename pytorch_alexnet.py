fd79da80f29e5f00b6866099af56e331d2a83a1e
pos0637 <pos0637@163.com>
2020-07-24 15:43:31 +0800

    添加pytorch alexnet mnist实现。

--- pytorch_alexnet.py
+++ pytorch_alexnet.py
@@ -1,145 +1,170 @@
 # -*- coding: UTF-8 -*-
 
-import torch  # 导入pytorch
-from torch import nn, optim  # 导入神经网络与优化器对应的类
+import argparse
+import torch
+import torch.nn as nn
 import torch.nn.functional as F
-from torchvision import datasets, transforms  # 导入数据集与数据预处理的方法
-import matplotlib.pyplot as plt
-
-# 数据预处理：标准化图像数据，使得灰度数据在-1到+1之间
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
-])
-
-# 下载Fashion-MNIST训练集数据，并构建训练集数据载入器trainloader,每次从训练集中载入64张图片，每次载入都打乱顺序
-trainset = datasets.FashionMNIST(
-    './dataset/', download=True, train=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=64, shuffle=True)
-
-# 下载Fashion-MNIST测试集数据，并构建测试集数据载入器trainloader,每次从测试集中载入64张图片，每次载入都打乱顺序
-testset = datasets.FashionMNIST(
-    './dataset/', download=True, train=False, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+from PIL import Image
 
 
-class Classifier(nn.Module):
-    def __init__(self):
-        super().__init__()
+class AlexNet(nn.Module):
 
-        # 卷积层
-        self.conv = nn.Sequential(
-            # 第一层卷积
-            # 如果使用mnist类的数据集，输入通道为1，如果使用的cifar等彩色图像的数据集，输入通道为3
-            nn.Conv2d(1, 96, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(),
+    def __init__(self, num_classes=1000):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
-
-            # 第二层卷积，开始减小卷积窗口
-            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
-
-            # 第三层卷积
-            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-
-            # 第四层卷积
-            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-
-            # 第五层卷积
-            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2)
         )
-
-        self.fc = nn.Sequential(
-            # 第一层全连接层
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
             nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-
-            # 第二层全连接层
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
             nn.Linear(4096, 4096),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-
-            # 第三层全连接层（输出层）
-            nn.Linear(4096, 10)
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
         )
 
     def forward(self, x):
-        feature = self.conv(x)
-        x = self.fc(feature.view(x.shape[0], -1))
-
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
         return x
 
 
-def evaluate_accuracy(testloader, model):
-    test_loss = 0
-    accuracy = 0
+class MyMNIST(datasets.MNIST):
+    def __getitem__(self, index):
+        img, target = self.data[index], int(self.targets[index])
+        img = Image.fromarray(img.numpy(), mode='L').convert('RGB')
 
-    # 测试的时候不需要开自动求导和反向传播
-    with torch.no_grad():
-        # 关闭Dropout
-        model.eval()
+        if self.transform is not None:
+            img = self.transform(img)
 
-        for images, labels in testloader:
-            images, labels = images.to(device), labels.cuda()
-            results = model(images).argmax(dim=1)
-            accuracy += (results == labels).float().sum().cpu().item()
+        if self.target_transform is not None:
+            target = self.target_transform(target)
 
-        # 恢复Dropout
-        model.train()
-
-    return test_loss, accuracy
+        return img, target
 
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+preprocess = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225]),
+])
 
-# 对上面定义的Classifier类进行实例化
-model = Classifier().to(device)
 
-# 定义损失函数为负对数损失函数
-criterion = nn.CrossEntropyLoss().to(device)
-
-# 优化方法为Adam梯度下降方法，学习率为0.003
-optimizer = optim.Adam(model.parameters(), lr=0.01)
-
-# 对训练集的全部数据学习15遍，这个数字越大，训练时间越长
-epochs = 15
-
-# 将每次训练的训练误差和测试误差存储在这两个列表里，后面绘制误差变化折线图用
-train_losses, test_losses = [], []
-
-print('开始训练')
-for e in range(epochs):
-    running_loss = 0
-
-    # 对训练集中的所有图片都过一遍
-    for images, labels in trainloader:
-        images, labels = images.to(device), labels.cuda()
-
-        # 将优化器中的求导结果都设为0，否则会在每次反向传播之后叠加之前的
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
-
-        # 对64张图片进行推断，计算损失函数，反向传播优化权重，将损失求和
-        log_ps = model(images)
-        loss = criterion(log_ps, labels)
+        output = model(data)
+        loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        running_loss += loss.item()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            if args.dry_run:
+                break
 
-    test_loss, accuracy = evaluate_accuracy(testloader, model)
-    print("训练集学习次数: {}/{}.. " . format(e + 1, epochs),
-          "训练误差: {:.3f}.. " . format(running_loss / len(trainloader)),
-          "测试误差: {:.3f}.. " . format(test_loss / len(testloader)),
-          "模型分类准确率: {:.3f} ". format(accuracy / len(testloader)))
 
-plt.plot(train_losses, label='Training loss')
-plt.plot(test_losses, label='Validation loss')
-plt.legend()
-plt.show()
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            # sum up batch loss
+            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            # get the index of the max log-probability
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=14, metavar='N',
+                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
+                        help='learning rate (default: 1.0)')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--dry-run', action='store_true', default=False,
+                        help='quickly check a single pass')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device('cuda:0' if use_cuda else 'cpu')
+
+    kwargs = {'batch_size': args.batch_size}
+    if use_cuda:
+        kwargs.update({
+            'pin_memory': True,
+            'shuffle': True},
+        )
+
+    dataset1 = datasets.MNIST('./data', train=True,
+                       download=True, transform=preprocess)
+    dataset2 = datasets.MNIST('./data', train=False, transform=preprocess)
+    train_loader = torch.utils.data.DataLoader(dataset1, **kwargs)
+    test_loader = torch.utils.data.DataLoader(dataset2, **kwargs)
+
+    model = AlexNet(num_classes=10)
+    print(model)
+
+    model = model.to(device)
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader)
+        scheduler.step()
+
+    if args.save_model:
+        torch.save(model.state_dict(), "mnist_cnn.pt")
+
+
+if __name__ == '__main__':
+    main()
