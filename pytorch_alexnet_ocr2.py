fb22a02a78be3f279aeb22d6f761c209ea6d3682
pos0637 <pos0637@163.com>
2020-07-27 18:08:23 +0800

    添加测试AlexNet OCR代码。

--- pytorch_alexnet_ocr2.py
+++ pytorch_alexnet_ocr2.py
@@ -18,26 +18,26 @@ class AlexNet(nn.Module):
     def __init__(self, num_classes=1000):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
-            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
-            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
             nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
-            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
-            # nn.ReLU(inplace=True),
-            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
-            # nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
             nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
         )
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
-            nn.Dropout(0.5),
+            nn.Dropout(),
             nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
+            nn.Dropout(),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes),
@@ -116,7 +116,7 @@ def train():
 
 
 train()
-net = torch.load('ocr_normal.pkl')
+net = torch.load('ocr.pkl')
 net.eval()
 
 # 开始识别
