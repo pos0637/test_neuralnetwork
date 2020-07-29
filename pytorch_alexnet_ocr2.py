cf7e6ae13a3ed258d002e2be09a17651776852f4
pos0637 <pos0637@163.com>
2020-07-29 18:24:02 +0800

    添加测试代码。

--- pytorch_alexnet_ocr2.py
+++ pytorch_alexnet_ocr2.py
@@ -9,26 +9,58 @@ from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 import numpy as np
 import cv2
-from torchvision.transforms.transforms import Resize
+from torchvision.transforms.transforms import Grayscale, Resize
+from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
 
-# 定义网络结构
+
+def preprocess(path):
+    raw = Image.open(path)
+    image = ImageOps.grayscale(raw)
+    raw.close()
+
+    (w, h) = image.size
+    w = int(w * 227 / h)
+    resized = image.resize((w, 227), Image.BILINEAR)
+    image.close()
+
+    output = Image.new('L', (227, 227), (255))
+    output.paste(resized, (int((227 - w) / 2), 0))
+    resized.close()
+
+    return output
+
+
+def show_samples(example_data, example_targets):
+    for i in range(6):
+        plt.subplot(2, 3, i + 1)
+        plt.tight_layout()
+        plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+        plt.title("Ground Truth: {}".format(example_targets[i]))
+        plt.xticks([])
+        plt.yticks([])
+    plt.show()
 
 
 class AlexNet(nn.Module):
     def __init__(self, num_classes=1000):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.Conv2d(1, 96, kernel_size=11, stride=4, padding=0),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+
+            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+
+            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
@@ -46,34 +78,36 @@ class AlexNet(nn.Module):
     def forward(self, x):
         x = self.features(x)
         x = self.avgpool(x)
-        x = torch.flatten(x, 1)
+        # x = torch.flatten(x, 1)
+        x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
 
 
 # transform
 transform = transforms.Compose([
-    transforms.Resize((227, 227)),
+    transforms.Grayscale(),
     # transforms.RandomHorizontalFlip(),
     # transforms.RandomGrayscale(),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                         std=[0.229, 0.224, 0.225]),
+    # transforms.Normalize((0.1307,), (0.3081,))
 ])
 
 transform1 = transforms.Compose([
-    transforms.Resize((227, 227)),
+    transforms.Grayscale(),
     transforms.ToTensor()
 ])
 
 # 加载数据
-trainset = datasets.ImageFolder(root='./samples', transform=transform)
+trainset = datasets.ImageFolder(
+    root='./samples', loader=preprocess, transform=transform)
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=64, shuffle=True, num_workers=0)
+    trainset, batch_size=16, shuffle=True, num_workers=0)
 
-testset = datasets.ImageFolder(root='./output', transform=transform1)
+testset = datasets.ImageFolder(
+    root='./output', loader=preprocess, transform=transform1)
 testloader = torch.utils.data.DataLoader(
-    testset, batch_size=64, shuffle=False, num_workers=0)
+    testset, batch_size=16, shuffle=False, num_workers=0)
 
 #device : GPU or CPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -87,14 +121,14 @@ def train():
     criterion = nn.CrossEntropyLoss()
 
     # 优化器 这里用SGD
-    optimizer = optim.SGD(net.parameters(), lr=1e-3, momentum=0.9)
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
     net.to(device)
     net.train()
 
     print("Start Training!")
 
-    num_epochs = 20  # 训练次数
+    num_epochs = 30  # 训练次数
 
     for epoch in range(num_epochs):
         for i, data in enumerate(trainloader):
@@ -114,6 +148,14 @@ def train():
     # 保存训练模型
     torch.save(net, 'ocr.pkl')
 
+
+examples = enumerate(trainloader)
+batch_idx, (example_data, example_targets) = next(examples)
+show_samples(example_data, example_targets)
+
+examples = enumerate(testloader)
+batch_idx, (example_data, example_targets) = next(examples)
+show_samples(example_data, example_targets)
 
 train()
 net = torch.load('ocr.pkl')
